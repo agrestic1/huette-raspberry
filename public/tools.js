b7202ec98db08bf7d54178cdbc9e5296eb5bb5d3
agrestic1 <fidel1ty@online.de>
2019-12-08 23:52:58 +0100

    hacknight #1

--- public/tools.js
+++ public/tools.js
@@ -1,19 +1,15 @@
 // Comunication between front and backend
 var socket = io.connect(); //load socket.io-client and connect to the host
-// function I2C_LED(data) {
-//     socket.emit("state", data); //send button state to server
-// }
-// function Websocket_LED(data) {
-//     socket.emit("websocket", data); //send button state to server
-// }
 
-// Toggle swicth
+var devices = {};
+
+// Toggle swicth arduino
 var arduino_input = document.getElementById('arduino_switch');
 var arduino_outputtext = document.getElementById('arduino_status');
 
 arduino_input.addEventListener('change', function() {
     if (this.checked) {
-        socket.emit("state", 1); //send button state to server
+        socket.emit("whiteLED/stateChange", { id: "2AAgbd_customID" }); //send button state to server
         arduino_outputtext.innerHTML = "an";
     } else {
         socket.emit("state", 0); //send button state to server
@@ -27,10 +23,10 @@ var ESP_outputtext = document.getElementById('ESP_status');
 
 ESP_input.addEventListener('change', function() {
     if (this.checked) {
-        socket.emit("websocket", "D" + slider.value); //send button state to server
+        socket.emit("Socket2", "D" + slider.value); //send button state to server
         ESP_outputtext.innerHTML = "an";
     } else {
-        socket.emit("websocket", "LED0"); //send button state to server
+        socket.emit("Socket2", "LED0"); //send button state to server
         ESP_outputtext.innerHTML = "aus";
     }
 });
@@ -43,5 +39,32 @@ output.innerHTML = slider.value;
 slider.oninput = function() {
     ESP_input.checked = true;
     output.innerHTML = this.value; // Turn switch on, when slider is moved
-    socket.emit("websocket", "D" + this.value); //send button state to server
+    socket.emit("Socket2", "D" + this.value); //send button state to server
 }
+
+// Add events from backend
+
+socket.on("attach", (data) => {
+    //devices.push(new Device({ id: data.id, type: data.type, ioHandle: socket }));
+    console.log(data);
+});
+
+socket.on("detach", (data) => {
+    console.log(data);
+});
+
+// class Device {
+//     _id;
+//     _type;
+//     _handle;
+//     constructor({ id, type, ioHandle }) {
+//         _id = id;
+//         _type = type;
+//         _handle = ioHandle;
+//     }
+
+//     onClick() {
+//         this._handle.emit(_type + "/stateChange", { id: _id }); //send button state to server
+//     }
+
+// }
--- webserver_key_works_not.js
+++ webserver_key_works_not.js
@@ -9,48 +9,40 @@ app.get('/', function(req, res) {
 app.use(express.static('public')); // express public folder
 http.listen(8080); //listen to port 8080
 
-var clients = {}; // not yet necessary to: Store clients
+//var clients = {}; // not yet necessary to: Store clients
 //var clitenIDs = [];  // not yet necessary to: Store clients
 var devices = {}; // List of devices
-//var deviceIDs = []; // List of ISs of connected devices
+var deviceIDs = []; // List of ISs of connected devices
 
 // I2C for communication with arduino
 const i2c = require('i2c-bus');
 const MCP9808_ADDR = 0x05;
 
-var callback = (socket) => { // Socket Connection to client
-    clients[socket.id] = socket; // not yet necessary to: Store clients
+var io = require('socket.io')(http) //require socket.io module and pass the http object
+io.sockets.on('connection', function(socket) { // Socket Connection to client
+    //clients[socket.id] = socket; // not yet necessary to: Store clients
     console.log('New Client with ID', socket.id);
     socket.on('state', function(data) { //get content from function "state" from client
         const i2c1 = i2c.openSync(1);
         console.log('Sending to arduino:', data);
         const rawData = i2c1.sendByteSync(MCP9808_ADDR, data);
         i2c1.closeSync();
-    });
-    socket.on('Socket2', (data) => { //get content from function "websocket"  from client
+    })
+    socket.on('websocket', function(data) { //get content from function "websocket"  from client
         // Websocket
-        console.log('Sending to Socket2:', data);
-        Object.keys(devices).forEach((element) => {
-            devices[element].emit("event", data);
-        });
-    });
-    socket.on('disconnect', () => { // Do this if on client disconnetcs
+        console.log('Sending to arduino:', data);
+        for (var i = deviceIDs.length - 1; i >= 0; i--) {
+            devices[deviceIDs[i]].emit("event", data);
+        }
+    })
+    socket.on('disconnect', function() { // Do this if on client disconnetcs
         console.log('Disconnection of ID', socket.id);
-        delete clients[socket.id]; // not yet necessary to: Store clients
-    });
+        //delete clients[socket.id]; // not yet necessary to: Store clients
+    })
+});
 
-    Object.keys(devices).forEach((element) => {
-        socket.emit("attach", { id: element, type: "LED" });
-    });
 
-}
-
-// first socket to communicate with clients
-var io = require('socket.io')(http) //require socket.io module and pass the http object
-io.sockets.on('connection', callback);
-
-// ------------------------------- DEVICE SOCKET ----------------------------------------
-// Socket2 to communicate with devices
+// second socket to communicate with devices
 var app2 = require('http').createServer()
 var io2 = require('socket.io')(app2);
 app2.listen(8081); // this one listens to 8081
@@ -59,21 +51,14 @@ io2.on('connection', function(socket) {
     console.log('New Device with ID', socket.id);
     socket.on("jsonObject", function(data) {
         console.log(data);
-    });
-
-    Object.keys(clients).forEach((element) => {
-        clients[element].emit("attach", { id: socket.id, type: "LED" });
-    });
-
+    })
     devices[socket.id] = socket; // Store object to make it available for other functions
-    //deviceIDs.push(socket.id); // And store its ID in an array
+    //deviceIDs.push(socket.id); // And store its it in an array
+    deviceIDs = devices.keys();
+    console.log(deviceIDs);
     socket.on('disconnect', function() { // Do this if a device disconnetcs
         console.log('Disconnection of ID', socket.id);
-
-        Object.keys(clients).forEach((element) => {
-            clients[element].emit("detach", { id: socket.id, type: "LED" });
-        });
-
         delete devices[socket.id]; // delete element
-    });
+        deviceIDs = devices.keys();
+    })
 });
